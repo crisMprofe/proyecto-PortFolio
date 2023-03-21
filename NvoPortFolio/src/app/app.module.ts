4e572c116c4b5f83863717759e062aba725be513
crisbmaia.profe@gmail.com <97813948+Taty71@users.noreply.github.com>
2023-03-21 16:50:26 -0300

    Agregando componetes, bootstrap y css - experiencia estudios habilidades banner infopersonal

--- NvoPortFolio/src/app/app.module.ts
+++ NvoPortFolio/src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './componentes/header/header.component';
 import { LogosComponent } from './componentes/logos/logos.component';
+import { BannerComponent } from './componentes/banner/banner.component';
+import { EstudiosComponent } from './componentes/estudios/estudios.component';
+import { InfopersonalComponent } from './componentes/infopersonal/infopersonal.component';
+import { ExperienciaComponent } from './componenetes/experiencia/experiencia.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogosComponent
+    LogosComponent,
+    BannerComponent,
+    EstudiosComponent,
+    InfopersonalComponent,
+    ExperienciaComponent,
+    HabilidadesComponent
   ],
   imports: [
     BrowserModule,
