41e96788e8d6eb527b02de3c38b05a854f18c2a7
crisbmaia.profe@gmail.com <97813948+Taty71@users.noreply.github.com>
2023-03-22 23:20:41 -0300

    varios cambios, agrega proyectos y footer

--- NvoPortFolio/src/app/app.module.ts
+++ NvoPortFolio/src/app/app.module.ts
@@ -3,13 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+
 import { HeaderComponent } from './componentes/header/header.component';
 import { LogosComponent } from './componentes/logos/logos.component';
 import { BannerComponent } from './componentes/banner/banner.component';
 import { EstudiosComponent } from './componentes/estudios/estudios.component';
 import { InfopersonalComponent } from './componentes/infopersonal/infopersonal.component';
-import { ExperienciaComponent } from './componenetes/experiencia/experiencia.component';
 import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
+import { NgCircleProgressModule } from 'ng-circle-progress';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { FooterComponent } from './componentes/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -19,12 +24,16 @@ import { HabilidadesComponent } from './componentes/habilidades/habilidades.comp
     BannerComponent,
     EstudiosComponent,
     InfopersonalComponent,
-    ExperienciaComponent,
-    HabilidadesComponent
+
+    HabilidadesComponent,
+     ExperienciaComponent,
+     ProyectosComponent,
+     FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgCircleProgressModule.forRoot({}),
   ],
   providers: [],
   bootstrap: [AppComponent]
